8fa84b44856767b5ebee7fb532d66144ba2eeed8
Tyler Mayoff <tyler@tylermayoff.com>
2024-03-07 21:58:13 -0500

    Added ros flake

---
+++ ros/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:lopsided98/nixpkgs/nix-ros";
+    flake-utils.url = "github:numtide/flake-utils";
+    ros-overlay.url = "github:lopsided98/nix-ros-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ros-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ros-overlay.overlays.default ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; with pkgs.rosPackages.humble; [
+            (buildEnv {
+              paths = [
+                ament-cmake-core
+                python-cmake-module
+                ros-core
+                vcstool
+                colcon
+              ];
+            })
+          ];
+        };
+      });
+}
