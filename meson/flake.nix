8a4efcc14a3b390c5ef0d2da6798d3c0e8aac361
Tyler Mayoff <tyler@tylermayoff.com>
2024-10-11 21:19:00 -0400

    Fleshed out the meson template

--- meson/flake.nix
+++ meson/flake.nix
@@ -3,24 +3,27 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import (nixpkgs) {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
           inherit system;
         };
       in {
-
-        devShell = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+        devShell = pkgs.mkShell.override {stdenv = pkgs.clangStdenv;} {
           nativeBuildInputs = with pkgs; [
             clang-tools
 
             meson
+            muon
             ninja
           ];
 
-          buildInputs = with pkgs; [
-          ];
+          buildInputs = [];
         };
       }
     );
